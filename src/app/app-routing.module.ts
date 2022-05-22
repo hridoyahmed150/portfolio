2465f85d68c04edb547ff25ccf33ce955fbad49d
Hridoy Ahmed <hridoyahmed150@gmail.com>
2022-05-23 05:42:19 +0600

    remove some file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlankLayoutComponent } from './layout/blank-layout/blank-layout.component';
 import { NotFoundComponent } from './layout/not-found/not-found.component';
 
 const routes: Routes = [
